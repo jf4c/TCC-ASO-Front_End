c7eeb6f2118db7d90d6bc7540fd78a04e19348c3
Julio Felipe <julio.f4costa@hotmail.com>
2025-11-05 17:34:53 -0300

    feat: created footer

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { HeaderComponent } from './components/header/header.component'
+import { FooterComponent } from './components/footer/footer.component'
 
 @Component({
   selector: 'aso-layout',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
